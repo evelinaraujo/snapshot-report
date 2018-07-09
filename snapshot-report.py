39392a4e4029d7c6ed0d17be852df96b17068d18
Evelin Araujo <earaujo@pnmac.com>
2018-07-08 22:58:12 -0700

    updates to snapshot-report.

--- snapshot-report.py
+++ snapshot-report.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-from datetime import datetime, timedelta
+import json
+from datetime import datetime, timedelta, date
 import logging
 import boto3
 #import csv
@@ -9,36 +10,41 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 tag_key='Name'
-tag_value = 'Evelin-Test'
 account='061682043522'
+client = boto3.client('ec2')
 
-def describe_snapshot(ec2, snapshot):
-    client = boto3.client('ec2')
-    response = client.describe_snapshots(
-        OwnerIds = [account]
-    )
-    number = len(response['Snapshots'])
-    print("There are a total of %s Snapshots in account %s " % (number, account) )
-    for snapshot in response['Snapshots']:
-        print snapshot['VolumeId']
-        for volume in snapshot['VolumeId']:
-            response2 = client.describe_volumes()
-            print volume 
-            # for volume['Volumes']:
-            #     print(volume['Attachments'])
+def describe_snapshot():
+    snapshot = client.describe_snapshots(
+        OwnerIds = [account])['Snapshots']
+    number = len(snapshot)
+    print("There are a total of %s Snapshots in account %s " % (number, account))    
+    for snap in snapshot:
+        snapid = snap['SnapshotId']
+        age = snap['StartTime'].strftime('%F')
+        print ("Snapshot %s was created on %s" % (snapid, age))
 
-    # for snapshot in response['Snapshots']:
-    #     response2 = ec2.describe_volume ()
-    #     print h
-        # if response2['Volumes']['Attachments']
+def describe_volumes():
+    volume = client.describe_volumes()
+    for v in volume['Volumes']:
+        if v['Attachments'] == []:
+            volumeid = v['VolumeId']
+            print ("The following volumes are not attached to an instance %s" % volumeid)
+        else:
+            for i in v['Attachments']:
+                instanceid = i['InstanceId']
+                instance = client.describe_instances(InstanceIds = [instanceid])['Reservations']
+                for i in instance:
+                    for l in i['Instances']:
+                        if l['Tags'] == []:
+                            print("Volume %s is attached to instance ID %s with no tag name" % (volumeid, instanceid))
+                        else:
+                            for m in l['Tags']:
+                                if m['Key'] == 'Name':
+                                    name = m['Value']
+                                    print ("Volume %s  is attached to instance name %s %s" % (volumeid, name, instanceid))
+                                
 
-        
-# with open('snapshot-report.csv', 'wb')
-
-
-        # Filters = [
-        #     {
-        #         'Name': 'tag:' + tag_key,
-        #         'Values': [tag_value]
-        #     }
-        # ]
+def lambda_handler(event,context):
+    describe_snapshot()
+#    return age, number, snapshot
+    describe_volumes()
